6ab3f2d5f482fc2f1687170b881c8f07d481d525
RSReitz <notthedba@outlook.com>
2017-09-01 12:14:35 -0400

    Added several controller actions to demonstrate different types of AcionResults.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,12 +9,56 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" }; 
+            var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+        }
+
+        public ActionResult Add(string name)
+        {
+
+            if (String.IsNullOrWhiteSpace(name))
+                name = "[enter movie]";
+
+            return Content("name=" + name);
+
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return HttpNotFound();
+            else
+            if (id < 1)
+                return RedirectToAction("Add", "Movies", new { name = "[not found, enter movie]" });
+            else
+                return Content("id=" + id);
+        }
+
+
+        public ActionResult View(int? id)
+        {
+            if (!id.HasValue)
+                return new EmptyResult();
+            else
+                return Content("id=" + id);
+        }
+
+        // Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
