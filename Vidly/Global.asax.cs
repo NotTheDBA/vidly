678eb63e895ca5b8b19efc820edde6b5056542ec
RSReitz <notthedba@outlook.com>
2017-09-12 17:52:39 -0400

    Added ASP.Net Identity framework.

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Vidly.Models;
+using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ using System.Web.Routing;
 
 namespace Vidly
 {
+    // Note: For instructions on enabling IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=301868
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
