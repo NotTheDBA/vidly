0c1184cab378194a66d53e4cc54258b456846576
RSReitz <notthedba@outlook.com>
2017-10-12 09:17:03 -0400

    Added New Customer form.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,15 +11,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [DisplayName("Subscribe")]
+
+        [Display(Name = "Subscribe")]
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Typee")]
         public byte MembershipTypeId { get; set; }
-        //[DataType(DataType.Date)]
-        [DisplayName("Date of Birth")]
+ 
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
     }
 }
