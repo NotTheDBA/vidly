44a3ff8dac20f8feb0b3d584411c0e4bbd06b744
RSReitz <notthedba@outlook.com>
2017-10-16 14:54:35 -0400

    Ooops - and validate the anti-forgery token.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -78,6 +78,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
