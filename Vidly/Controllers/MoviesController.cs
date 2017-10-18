4cb08fdcea50a225a2ab1dbf3e092dca7e512559
RSReitz <notthedba@outlook.com>
2017-10-18 11:28:34 -0400

    Added anti-forgery token to the Movies form.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -105,6 +105,7 @@ namespace Vidly.Controllers
 
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
